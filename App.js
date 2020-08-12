59df90b96e33f1eeddfa043292ebb43e79049394
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-08-12 18:02:28 +0530

    Fonts

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Screen from './navigation/Screen';
 
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default function App() {
+
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+
+  
   return <Screen />
 }
 
