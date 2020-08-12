202242236c909be588f29b2c6ffd1704b68272d5
Anurag-ak99 <anurag.ch98@gmail.com>
2020-08-12 18:35:17 +0530

    Update Colors.js

--- Items/Colors.js
+++ Items/Colors.js
@@ -1,6 +1,15 @@
-import { color } from "react-native-reanimated";
-
 export default{
   primary1: '#011131',
+  accent: '#EF2327',
+  primary2: '#2D375B',
+  primary3: '#596188',
+  secondary1: '#14161B',
+  secondary2: '#3A3C42',
+  secondary3: '#64666D',
 
+  //colors for button
+  button: '#000000',
+  large: '#FFFFFF',
+  disabledbutton: '#BDBDBD',
+  textbutton: '#2D375B'
 }
