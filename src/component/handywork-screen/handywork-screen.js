05eee102041b22c7558743226b282fd96ad34a3b
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-08-22 08:44:17 +0530

    HandyWork
    Design my HAndu work

--- src/component/handywork-screen/handywork-screen.js
+++ src/component/handywork-screen/handywork-screen.js
@@ -1,119 +1,113 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, } from 'react-native';
 
 import Dark_Button from '../../Items/Buttons/dark-bt';
-import colors from '../../Items/Colors';
+import Colors from '../../Items/Colors';
 
-const HandyWork_Screen = props => {
+
+const ProfileScreen = props => {
     return(
+
     <View style={styles.screen} >
-      <View style={styles.container1}>
-        <Text style={styles.head}>HandyWork</Text>
-      </View>
-      <View style={styles.container2}>
-        <Image 
-          source={require('../h2h/s7.png')}
-          resizeMode= 'contain'
-          // resizeMethod= 'auto'
-          style={{
-            width: '60%',
-            height: '95%',
-            alignSelf: 'center',     
-          }}
-         />
-      </View>
-      <View style={styles.container3} >
+    <View style={styles.container1}>
+        <Text style={styles.head}> HandyWork</Text>
+    </View>
+    <View style={styles.container2}>
+      <Image 
+      source={require('../h2h/s7.png')}
+      resizeMode= 'contain'
+      style={{
+        width: '57%',
+        height: '100%',
+        alignSelf: 'center'
+      }}
+       />
+    </View>
+    <View style={styles.container3}>
         <Text style={styles.head2}> Lawn too tall? Garder Filled with weeds? </Text> 
         <Text style={styles.head2}> Drain plugged? someone near by has a</Text> 
         <Text style={styles.head2}> hand to offer.</Text> 
-      </View>
-      <View style={styles.container4}>
-       <Text style={styles.head3}>Select type of Handywork</Text> 
-      </View>
-      <View style={styles.container5}>
-       <Text style={{alignSelf: 'center'}}> development</Text>
       
-      </View>
-      <View style={styles.container6}>
-       <Dark_Button>
-         <Text style={{fontWeight: 'bold'}}>
-         Request For HandyWork
-         </Text>
-       </Dark_Button>
-      </View>
-     
-        
     </View>
-    );
-}
+    <View style={styles.container4}>
+      <Text style={styles.head3}>Select type of Handywork</Text> 
+    </View>
+    <View style={styles.container5}>
+     <Text style={{alignSelf: 'center'}}>on dev</Text> 
+    </View>
+    <View style={styles.container6}>
+       <Dark_Button>
+         <Text> HandyWork</Text>
+       </Dark_Button>
+    </View>
+    </View>
+    );  
+};
 
 const styles = StyleSheet.create({
     screen: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      margin: '2%',
-      paddingBottom: '5%'
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '1.8%'
     },
-    container1: {
+    container1:{
       flex: 1,
       width: '100%',
-      alignSelf: 'flex-start',
-      paddingHorizontal: '4%',
+      // backgroundColor: '#C6C438',
     },
-    head: {
-      color: colors.primary1,
-      fontSize: 26,
-      fontWeight: "bold"
-    },
-    container2: {
-      flex: 2.3,
+    container2:{
+      flex:2.3,
       width: '100%',
+      // backgroundColor: '#A596D3',
       
     },
-    container3: { 
+    container3:{
       flex: 1.3,
       flexDirection: 'column',
       width: '100%',
       paddingHorizontal: '2%',
       paddingTop: '5%',
-    
-     
-     
     },
-    container4: { 
-      flex: 1,
+    container4:{
+      flex:1.8,
       width: '100%',
-      flexDirection: 'column',
-      justifyContent :'center',
-      paddingVertical: '1.8%',
-    },
-    container5: { 
-      flex: 4,
       flexDirection: 'column',
       justifyContent: 'center',
-      // backgroundColor: '#1630B0',
+      // backgroundColor: '#267DA5',
+    },
+    container5:{
+      flex:4,
       width: '100%',
+      flexDirection: 'column',
+      justifyContent:'center',
+      alignSelf: 'center'
+      // backgroundColor: '#86EC4F',
     },
-    container6: { 
-      flex: 1.4,
-      // backgroundColor: '#218291',
-      width: '100%'
+    container6:{
+      flex:1.7,
+      width: '100%',
+      // backgroundColor: '#9811C9',
+      paddingHorizontal: '2%',
+      paddingTop: '5%'
     },
-    head2: {
-      alignSelf: 'flex-start',
-      fontSize: 18,
-      color: colors.primary2,
-  
-    },
-    head3: {
-      fontSize: 17.5,
-      alignSelf: 'center',
-      fontWeight: 'bold',
-    }
+   head: {
+     fontSize: 26,
+     fontWeight: 'bold',
+     color: Colors.primary1
+   },
+   head2: {
+     alignSelf: 'flex-start',
+     fontSize: 18,
+     color: Colors.primary2
+   },
+   head3: {
+    fontSize: 17.5,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  }
 
-   
 });
 
-export default HandyWork_Screen;
+export default ProfileScreen;
