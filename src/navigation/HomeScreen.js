73fc4da018eb11fdfb72006e6f58266dbc904817
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-08-26 23:43:20 +0530

    cc

--- src/navigation/HomeScreen.js
+++ src/navigation/HomeScreen.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,108 +7,99 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Loadscreen from '../component/home/Load'; 
 import Volunteer  from '../component/Volunteer'; 
 import Victim  from '../component/Help-need';
-
 import Social_Screen from '../component/socialise/socialise-screen';
-import Social_Screen2 from '../component/socialise/socialise-screen2';
-
 import Pet_Screen from '../component/pet-care/pet';
 import Pet_screen2 from "../component/pet-care/petCare2";
-
 import HandyWork_Screen from '../component/handywork-screen/handywork-screen';
-
 import Transport_screen from '../component/transportation/transportation';
-import Transport_screen2 from '../component/transportation/transportation2';
-=======
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
-import Loadscreen from "../component/home/Load";
-import Volunteer from "../component/Volunteer";
-import Victim from "../component/Help-need";
-import Social_Screen from "../component/socialise/socialise-screen";
-import Pet_Screen from "../component/pet-care/pet";
-import Pet_screen2 from "../component/pet-care/petCare2";
-import HandyWork_Screen from "../component/handywork-screen/handywork-screen";
-import Transport_screen from "../component/transportation/transportation";
->>>>>>> Stashed changes
-
-import Chat_Screen from "../servises/chat/chat";
-import Browsing_Screen from "../servises/browsing-requests/browsing-screen";
-import ProfileScreen from "../component/profile/profile";
+import Chat_Screen from '../servises/chat/chat';
+import Browsing_Screen from '../servises/browsing-requests/browsing-screen';
+import ProfileScreen from '../component/profile/profile';
 
 const ScreenConstant = createStackNavigator();
 
+
+
+
+
 function SocialScreen() {
-  return (
-    <ScreenConstant.Navigator>
+  return(
+    <ScreenConstant.Navigator >
       <ScreenConstant.Screen name="socialscreen1" component={Social_Screen} />
-      <ScreenConstant.Screen name="socialscreen2" component={Social_Screen2} />
     </ScreenConstant.Navigator>
   );
-}
+};
 
 function Petscreen() {
-  return (
-    <ScreenConstant.Navigator>
+  return(
+    <ScreenConstant.Navigator >
       <ScreenConstant.Screen name="PetScreen1" component={Pet_Screen} />
       <ScreenConstant.Screen name="PetScreen2" component={Pet_screen2} />
     </ScreenConstant.Navigator>
   );
-}
+};
 
 function HandyWorkScreen() {
-  return (
-    <ScreenConstant.Navigator>
-      <ScreenConstant.Screen
-        name="handyWorkScreen1"
-        component={HandyWork_Screen}
-      />
+  return(
+    <ScreenConstant.Navigator >
+      <ScreenConstant.Screen name="handyWorkScreen1" component={HandyWork_Screen} />
     </ScreenConstant.Navigator>
   );
-}
+};
 
 function TransportScreen() {
-<<<<<<< Updated upstream
   return(
     <ScreenConstant.Navigator >
       <ScreenConstant.Screen name="transport_screen1" component={Transport_screen} />
-      <ScreenConstant.Screen name="transport_screen2" component={Transport_screen2} />
-=======
-  return (
-    <ScreenConstant.Navigator>
-      <ScreenConstant.Screen
-        name="transport_screen1"
-        component={Transport_screen}
-      />
->>>>>>> Stashed changes
     </ScreenConstant.Navigator>
   );
-}
+};
+
+
 
 function HomeScreen() {
   return (
     <NavigationContainer>
-      <ScreenConstant.Navigator screenOptions={{ headerShown: false }}>
+      <ScreenConstant.Navigator screenOptions={{headerShown: false}}>
         <ScreenConstant.Screen name="Home" component={Loadscreen} />
         <ScreenConstant.Screen name="vol" component={Volunteer} />
         <ScreenConstant.Screen name="Victim" component={Victim} />
         <ScreenConstant.Screen name="social_screen" component={SocialScreen} />
         <ScreenConstant.Screen name="pet_screen" component={Petscreen} />
-        <ScreenConstant.Screen
-          name="handywork_screen"
-          component={HandyWorkScreen}
-        />
-        <ScreenConstant.Screen
-          name="transport_screen"
-          component={TransportScreen}
-        />
+        <ScreenConstant.Screen name="handywork_screen" component={HandyWorkScreen} />
+        <ScreenConstant.Screen name="transport_screen" component={TransportScreen} />
         <ScreenConstant.Screen name="chat" component={Chat_Screen} />
         <ScreenConstant.Screen name="browse" component={Browsing_Screen} />
         <ScreenConstant.Screen name="profile" component={ProfileScreen} />
+        
       </ScreenConstant.Navigator>
+     
     </NavigationContainer>
   );
 }
 
 export default HomeScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
