d12bc5f1b78d1600452dcd5033448abd1769c225
apoorv-v <akshatsharma1699@gmail.com>
2020-08-30 22:18:49 +0530

    cc

--- src/Items/Buttons/dark-bt2.js
+++ src/Items/Buttons/dark-bt2.js
@@ -4,10 +4,8 @@ import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 const Dark_Button2 = (props) => {
   return (
     <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-      <View style={{ alignItems: "flex-end" }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>{props.children}</Text>
-        </View>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.children}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -19,7 +17,6 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     borderRadius: 30,
     alignItems: "center",
-    width: "35%",
   },
   buttonText: {
     color: "white",
