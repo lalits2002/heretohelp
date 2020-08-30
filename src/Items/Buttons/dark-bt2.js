b6506fcb1a4b4d6e01c305ea877e51e044b34d76
apoorv-v <akshatsharma1699@gmail.com>
2020-08-30 22:05:27 +0530

    updated dark btn in source file

--- src/Items/Buttons/dark-bt2.js
+++ src/Items/Buttons/dark-bt2.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 
-
-const Dark_Button2 = props => {
-    return (
-      <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+const Dark_Button2 = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+      <View style={{ alignItems: "flex-end" }}>
         <View style={styles.button}>
           <Text style={styles.buttonText}>{props.children}</Text>
         </View>
-      </TouchableOpacity>
-    );
-  };
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-
-
-  
-  const styles = StyleSheet.create({
-    button: {
-      backgroundColor: '#2D375B',
-      paddingVertical: 15,
-      borderRadius: 30,
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 19
-    }
-  });
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2D375B",
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: "center",
+    width: "35%",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 19,
+  },
+});
 
 export default Dark_Button2;
-
-  
