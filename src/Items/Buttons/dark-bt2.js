7eee9ba280e3a8cc9c481885d6d61ae176d09224
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-08-31 15:43:11 +0530

    cc

---
+++ src/Items/Buttons/dark-bt2.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+
+const Dark_Button2 = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2D375B",
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 19,
+  },
+});
+
+export default Dark_Button2;
