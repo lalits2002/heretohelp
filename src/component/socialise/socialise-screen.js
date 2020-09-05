7eef9a2f35745d1dc2c24cb477025ba6a4df6214
Ashutosh2002 <ashutoshswarup2002@gmail.com>
2020-09-05 13:12:09 +0530

    Socialise-Screen2
    Light button font needs to be changed
    and also background color

--- src/component/socialise/socialise-screen.js
+++ src/component/socialise/socialise-screen.js
@@ -1,139 +1,140 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Feather } from '@expo/vector-icons';
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
 // import Picker from './picker';
-import Colors from '../../Items/Colors';
-import Dark_Button from '../../Items/Buttons/dark-bt';
+import Colors from "../../Items/Colors";
+import Dark_Button from "../../Items/Buttons/dark-bt";
 
-const Social_Screen = props => {
-    const navigation = useNavigation();
+const Social_Screen = (props) => {
+  const navigation = useNavigation();
 
-
-    return (
-        <View style={{ ...styles.screen, ...props.style}}>
-      <View style={styles.top} >
-        <TouchableOpacity  onPress={() => {navigation.goBack()}} >
-          <Text style={styles.back}><Feather name="chevron-left" size={15} color= {Colors.primary3} />Back</Text>
+  return (
+    <View style={{ ...styles.screen, ...props.style }}>
+      <View style={styles.top}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Text style={styles.back}>
+            <Feather name="chevron-left" size={15} color={Colors.primary3} />
+            Back
+          </Text>
         </TouchableOpacity>
       </View>
 
-
-        <View style={styles.container1}>
+      <View style={styles.container1}>
         <Text style={styles.head}>Socialise</Text>
-        </View>
-        <View style={styles.container2}>
-            <Image
-            source={require('../h2h/s5.png')}
-            resizeMode= 'contain'
-            style={{
-                width: '100%',
-                height: '100%',
-                alignSelf: 'center'
-            }}
-         />
+      </View>
+      <View style={styles.container2}>
+        <Image
+          source={require("../h2h/s5.png")}
+          resizeMode="contain"
+          style={{
+            width: "100%",
+            height: "100%",
+            alignSelf: "center",
+          }}
+        />
+      </View>
+      <View style={styles.container3}>
+        <Text style={styles.head2}>
+          Whether it be a phone call with a new friend, grabbing a coffee, or
+          seeking a new hiking buddy.
+        </Text>
+      </View>
 
-        </View>
-        <View style={styles.container3}>
-          <Text style={styles.head2}>Whether it be a phone call with a new friend, grabbing a coffee, or seeking a new hiking buddy.</Text>
-
-        </View>
-
-        <View style={styles.container4}>
+      <View style={styles.container4}>
         <Text style={styles.head3}>Select type of social activity</Text>
-
-        </View>
-        <View style={styles.container5}>
-          {/* <Picker /> */}
-        </View>
-        <View style={styles.container6}>
-            <Dark_Button onPress={() => props.navigation.navigate('socialscreen2')} >
-                <Text>Request for Socialise</Text>
-            </Dark_Button>
-
-        </View>
+      </View>
+      <View style={styles.container5}>{/* <Picker /> */}</View>
+      <View style={styles.container6}>
+        <Dark_Button onPress={() => props.navigation.navigate("socialscreen2")}>
+          <Text>Request for Socialise</Text>
+        </Dark_Button>
+      </View>
     </View>
-    );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        height:'100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '1.6%',
-    },
-    top: {
-        flex:0.85,
-        width: '100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignSelf: 'flex-start',
-        paddingTop: '3%'
-        // backgroundColor: "#A596D3",
-    },
-    container1:{
-        flex: 0.6,
-        width: '100%',
-        // backgroundColor: '#fcba03',
-    },
-    container2:{
-        flex:2.7,
-        width: '100%',
-        // backgroundColor: '#8a53b8',
-    },
-    container3:{
-        flex: 1.2,
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        width: '100%',
-        paddingHorizontal: "2%",
-        // backgroundColor: '#8af249',
-    },
-    container4:{
-        flex: 0.5,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        width: '100%',
-        paddingTop: '2%',
+  screen: {
+    flex: 1,
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "1.6%",
+  },
+  top: {
+    flex: 0.85,
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignSelf: "flex-start",
+    paddingTop: "3%",
+    // backgroundColor: "#A596D3",
+  },
+  container1: {
+    flex: 0.6,
+    width: "100%",
+    // backgroundColor: '#fcba03',
+  },
+  container2: {
+    flex: 2.7,
+    width: "100%",
+    // backgroundColor: '#8a53b8',
+  },
+  container3: {
+    flex: 1.2,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    width: "100%",
+    paddingHorizontal: "2%",
+    // backgroundColor: '#8af249',
+  },
+  container4: {
+    flex: 0.5,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    width: "100%",
+    paddingTop: "2%",
 
-        // backgroundColor: '#59e6f0',
-    },
-    container5:{
-        flex: 5,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        width: '100%',
-        // backgroundColor: '#b5ad98',
-    },
-    container6:{
-        flex: 0.8,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        width: '100%',
-        paddingHorizontal: '2%',
-        paddingBottom: '5%',
-        // backgroundColor: '#e83354',
-    },
-     head: {
-         fontSize: 25,
-         fontWeight: 'bold',
-         
-         color: Colors.primary1
-     },
-      head2: {
-          alignSelf: 'center',
-          fontSize: 20,
-          color: Colors.primary2
-      },
-      head3: {
-        fontSize: 18.5,
-        alignSelf: 'center',
-        fontWeight: 'bold',
-      }
+    // backgroundColor: '#59e6f0',
+  },
+  container5: {
+    flex: 5,
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "100%",
+    // backgroundColor: '#b5ad98',
+  },
+  container6: {
+    flex: 0.8,
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "100%",
+    paddingHorizontal: "2%",
+    paddingBottom: "5%",
+    // backgroundColor: '#e83354',
+  },
+  head: {
+    fontSize: 25,
+    fontWeight: "bold",
 
+    color: Colors.primary1,
+  },
+  head2: {
+    alignSelf: "center",
+    fontSize: 20,
+    color: Colors.primary2,
+  },
+  head3: {
+    fontSize: 18.5,
+    alignSelf: "center",
+    fontWeight: "bold",
+  },
 });
 
 export default Social_Screen;
--- src/component/socialise/socialise-screen2.js
+++ src/component/socialise/socialise-screen2.js
@@ -1,32 +1,133 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import Colors from "../../Items/Colors";
+import { useNavigation } from "@react-navigation/native";
+import Dark_Button2 from "../../Items/Buttons/dark-bt2";
+import Button2 from "../../Items/Buttons/light-bt";
 
-
-const Social_Screen2 = props => {
-    
-
-
-    return (
-    <View style={{ ...styles.screen, ...props.style}}>
-      <Text>Social_Screen</Text>
+const Social_Screen2 = (props) => {
+  const navigation = useNavigation();
+  return (
+    <View style={{ ...styles.screen, ...props.style }}>
+      <View style={styles.top}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Text style={styles.back}>
+            <Feather name="chevron-left" size={15} color={Colors.primary3} />
+            Back
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container1}>
+        <Text style={styles.head}>Socialise</Text>
+      </View>
+      <View style={styles.container2}>
+        <Text style={styles.head2}>Coffee meetup</Text>
+      </View>
+      <View style={styles.container3}>
+        <Image
+          source={require("../h2h/s5.png")}
+          resizeMode="contain"
+          style={{
+            width: "100%",
+            height: "100%",
+            alignSelf: "center",
+          }}
+        />
+      </View>
+      <View style={styles.container4}>
+        <Text style={styles.head3}>Select your preference</Text>
+      </View>
+      <View style={styles.container5}>
+        <Dark_Button2>
+          <Text>I am flexible with time and location</Text>
+        </Dark_Button2>
+      </View>
+      <View style={styles.container6}>
+        <Button2>
+          <Text>I have a specific time and location</Text>
+        </Button2>
+      </View>
     </View>
-    );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        height:'100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '1.6%',
-        paddingTop: '7%',
-        backgroundColor: '#ffffff',
-        
-    },
-    
+  screen: {
+    flex: 1,
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "1.6%",
+    paddingTop: "7%",
+    backgroundColor: "#ffffff",
+  },
+  top: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignSelf: "flex-start",
+    paddingTop: "3%",
+    // backgroundColor: "#A596D3",
+  },
+  container1: {
+    flex: 1,
+    width: "100%",
+    //backgroundColor: "#fcba03",
+  },
+  container2: {
+    flex: 1,
+    width: "100%",
+    //backgroundColor: "#A596D3",
+  },
+  container3: {
+    flex: 5,
+    width: "100%",
+    //backgroundColor: "#fcba03",
+  },
+  container4: {
+    flex: 1.5,
+    width: "100%",
+    //backgroundColor: "#A596D3",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  container5: {
+    paddingTop: "1%",
+    flex: 2,
+    width: "100%",
+    //backgroundColor: "#fcba03",
+  },
+  container6: {
+    flex: 5,
+    width: "100%",
+    //backgroundColor: "#A596D3",
+  },
+  head: {
+    fontSize: 25,
+    fontWeight: "bold",
 
+    color: Colors.primary1,
+  },
+  head2: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: Colors.primary2,
+  },
+  head3: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: Colors.primary2,
+    alignSelf: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
 });
 
 export default Social_Screen2;
