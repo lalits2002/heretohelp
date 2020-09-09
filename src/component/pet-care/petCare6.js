475c4551a7f23a98a0ae148690f10377381934a7
apoorv-v <akshatsharma1699@gmail.com>
2020-09-09 17:41:03 +0530

    keyboard issue/ do npm i

--- src/component/pet-care/petCare6.js
+++ src/component/pet-care/petCare6.js
@@ -1,33 +1,84 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  StatusBar,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useKeyboard } from "react-native-keyboard-height";
 
 import Dark_Button from "../../Items/Buttons/dark-bt";
 import Colors from "../../Items/Colors";
 import Top_container from "./head";
 
+const screenHeight = Dimensions.get("window").height;
+
 const Pet_screen6 = (props) => {
   const navigation = useNavigation();
   const [value, onChangeText] = useState("");
 
+  // for dynamic flex
+  const [topFlex, setTopFlex] = useState(5);
+  const [midFlex, setMidFlex] = useState(4);
+  const [bottomFlex, setBottomFlex] = useState(1);
+
+  //
+  const [kbHeight, setKbHeight] = useState(screenHeight);
+
+  const didShow = (height) => {
+    console.log("Keyboard show. Height is " + height);
+    // setViewHeight(screenHeight - height);
+    // height divide by height of container
+    setKbHeight(
+      height / screenHeight - StatusBar.currentHeight - 0.088 * screenHeight
+    );
+    //keyboard height ratio to the main height stored in const as not refreshed imm
+    const kbhFactor = kbHeight;
+    console.log(kbhFactor);
+
+    setBottomFlex(bottomFlex - (kbhFactor * bottomFlex) / 5);
+
+    setMidFlex(midFlex - (3 * kbhFactor * midFlex) / 4);
+  };
+
+  const didHide = () => {
+    console.log("Keyboard hide");
+    //giving default flex back
+    setBottomFlex(1);
+    setTopFlex(5);
+    setMidFlex(4);
+  };
+
+  const [keyboardHeigth] = useKeyboard(
+    didShow,
+    didHide
+  ); /* initialize the hook (optional parameters) */
+
+  useEffect(() => {}, [keyboardHeigth]);
+
   return (
     <View style={{ ...styles.screen, ...props.style }}>
-      <View style={styles.topContainer}>
+      <View style={topstyling(topFlex)}>
         <Top_container
           title="Pet Care"
           sub_head="Sitting"
           detail_2="What is your pet's name?"
         />
       </View>
-      <View style={styles.midContainer}>
+      <View style={midstyling(midFlex)}>
         <TextInput
+          autoCapitalize="sentences"
+          autoFocus={true}
           style={styles.input}
           onChangeText={(text) => onChangeText(text)}
           value={value}
           placeholder={"Enter Text"}
         />
       </View>
-      <View style={styles.bottomContainer}>
+      <View style={bottomstyling(bottomFlex)}>
         <Dark_Button
           onPress={() =>
             props.navigation.navigate("PetScreen7", {
@@ -43,6 +94,31 @@ const Pet_screen6 = (props) => {
   );
 };
 
+const topstyling = (dFlex) => {
+  return { flex: dFlex, width: "100%" };
+};
+const midstyling = (dflex) => {
+  return {
+    flex: dflex,
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignSelf: "center",
+    // backgroundColor: "#86EC4F",
+  };
+};
+const bottomstyling = (dFlex) => {
+  return {
+    flex: dFlex,
+    width: "100%",
+    // backgroundColor: "#9811C9",
+    justifyContent: "center",
+    paddingHorizontal: "2%",
+    paddingTop: "5%",
+    paddingLeft: "60%",
+  };
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
@@ -52,29 +128,6 @@ const styles = StyleSheet.create({
     paddingTop: "7%",
     padding: "1.8%",
   },
-  topContainer: {
-    flex: 5,
-    width: "100%",
-  },
-
-  midContainer: {
-    flex: 4,
-    width: "100%",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignSelf: "center",
-    // backgroundColor: "#86EC4F",
-  },
-  bottomContainer: {
-    flex: 1,
-    width: "100%",
-    // backgroundColor: "#9811C9",
-    justifyContent: "center",
-    paddingHorizontal: "2%",
-    paddingTop: "5%",
-    paddingLeft: "60%",
-  },
-
   input: {
     fontSize: 20,
     color: Colors.secondary3,
