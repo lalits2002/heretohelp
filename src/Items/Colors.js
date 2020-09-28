280289257cdb7d73c64f7e185ac75834504e52bc
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-09-28 13:39:42 +0530

    Merge branch 'profile' into main

--- src/Items/Colors.js
+++ src/Items/Colors.js
@@ -1,11 +1,13 @@
 export default{
   primary1: '#011131',
-  accent: '#EF2327',
   primary2: '#2D375B',
   primary3: '#596188',
+  primary4: '#D5DBF5',
   secondary1: '#14161B',
   secondary2: '#3A3C42',
   secondary3: '#64666D',
+  secondary4: '#4F4F4F',
+  accent: '#EF2327',
 
   //colors for button
   button: '#000000',
