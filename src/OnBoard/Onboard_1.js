d735f8e225c14e244c025015f886691fbf3b0ee8
liam eemag <or.samuel1@gmail.com>
2021-03-01 17:27:20 -0800

    feature(onboard-swipe) refactored onboard files and added swipe to next screen functionality

--- src/OnBoard/Onboard_1.js
+++ src/OnBoard/Onboard_1.js
@@ -1,63 +1,36 @@
 import React from "react";
-import { StyleSheet, View, Text, Image, SafeAreaView } from "react-native";
-import { Button } from 'react-native-elements';
+import { View, Image } from "react-native";
 import AppText from '../component/AppText/AppText';
 import styles from './OnboardStyles';
 
-const Onboard_screen = (props) => {
-
-  const nextScreen = () => {
-    props.navigation.navigate('Onboarding_2');
-  }
+const Onboard_screen = () => {
   
   return (
-    <SafeAreaView style={{ ...styles.screen, ...props.style }}>
-      <View style={styles.container}>
-        <View style={styles.top_navigation}>
-          <Button
-            type='clear'
-            title='Skip'
-            titleStyle={styles.top_nav_text}
-            onPress={nextScreen}
+    <View style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <View style={styles.header_image}>
+          <Image
+            source={require("./img/onb.png")}
+            resizeMode="contain"
+            style={{
+              width: "100%",
+              height: "100%",
+              alignSelf: "center",
+            }}
           />
         </View>
-
-        <View style={styles.header}>
-          <View style={styles.header_image}>
-            <Image
-              source={require("./img/onb.png")}
-              resizeMode="contain"
-              style={{
-                width: "100%",
-                height: "100%",
-                alignSelf: "center",
-              }}
-            />
-          </View>
-        
-          <View style={styles.title}>
-            <AppText style={styles.head}>Seek local volunteers</AppText>
-          </View>
+      
+        <View style={styles.title}>
+          <AppText style={styles.head}>Seek local volunteers</AppText>
         </View>
-
-        <View style={styles.content}>
-          <AppText style={styles.text}>
-            Ask for assistance! Get help with pet care, transportation and handywork!
-          </AppText>
-        </View>
-
-        <View style={styles.navigation}>
-          <View style={styles.navigation_control}>
-          </View>
-          <View style={styles.progress_bar_container}>
-            <View style={styles.progress_bar_filled}></View>
-            <View style={styles.progress_bar}></View>
-            <View style={styles.progress_bar}></View>
-          </View>
-        </View>
-
       </View>
-    </SafeAreaView>
+
+      <View style={styles.content}>
+        <AppText style={styles.text}>
+          Ask for assistance! Get help with pet care, transportation and handywork!
+        </AppText>
+      </View>
+    </View>
   );
 };
 
--- src/OnBoard/Onboard_2.js
+++ src/OnBoard/Onboard_2.js
@@ -1,63 +1,37 @@
 import React from "react";
-import { View, Image, SafeAreaView } from "react-native";
-import { Button } from 'react-native-elements';
+import { View, Image } from "react-native";
 import AppText from '../component/AppText/AppText';
 import styles from './OnboardStyles';
 
-const Onboard_screen = (props) => {
-
-  const nextScreen = () => {
-    props.navigation.navigate('Onboarding_3');
-  }
+const Onboard_screen = () => {
   
   return (
-    <SafeAreaView style={{ ...styles.screen, ...props.style }}>
-      <View style={styles.container}>
-        <View style={styles.top_navigation}>
-          <Button
-            type='clear'
-            title='Skip'
-            titleStyle={styles.top_nav_text}
-            onPress={nextScreen}
+    <View style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <View style={styles.header_image}>
+          <Image
+            source={require("./img/onb.png")}
+            resizeMode="contain"
+            style={{
+              width: "100%",
+              height: "100%",
+              alignSelf: "center",
+            }}
           />
         </View>
-
-        <View style={styles.header}>
-          <View style={styles.header_image}>
-            <Image
-              source={require("./img/onb.png")}
-              resizeMode="contain"
-              style={{
-                width: "100%",
-                height: "100%",
-                alignSelf: "center",
-              }}
-            />
-          </View>
-        
-          <View style={styles.title}>
-            <AppText style={styles.head}>Give back to the community</AppText>
-          </View>
+      
+        <View style={styles.title}>
+          <AppText style={styles.head}>Give back to the community</AppText>
         </View>
-
-        <View style={styles.content}>
-          <AppText style={styles.text}>
-            Become a volunteer and help locals with their tasks! 
-          </AppText>
-        </View>
-
-        <View style={styles.navigation}>
-          <View style={styles.navigation_control}>
-          </View>
-          <View style={styles.progress_bar_container}>
-            <View style={styles.progress_bar_filled}></View>
-            <View style={styles.progress_bar_filled}></View>
-            <View style={styles.progress_bar}></View>
-          </View>
-        </View>
-
       </View>
-    </SafeAreaView>
+
+      <View style={styles.content}>
+        <AppText style={styles.text}>
+          Become a volunteer and help locals with their tasks! 
+        </AppText>
+      </View>
+    </View>
+
   );
 };
 
